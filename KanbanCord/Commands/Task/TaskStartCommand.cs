3562810778ad5667eaf974620c0b25d541864306
Jasper Christiansen <jasper@jazper.dk>
2024-11-01 17:07:10 +0100

    Added task start command

---
+++ KanbanCord/Commands/Task/TaskStartCommand.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel;
+using DSharpPlus.Commands;
+using DSharpPlus.Commands.ArgumentModifiers;
+using DSharpPlus.Commands.Processors.SlashCommands;
+using DSharpPlus.Commands.Processors.SlashCommands.ArgumentModifiers;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using KanbanCord.Extensions;
+using KanbanCord.Helpers;
+using KanbanCord.Models;
+using KanbanCord.Providers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KanbanCord.Commands.Task;
+
+partial class TaskCommandGroup
+{
+    [Command("start")]
+    [Description("Start a task and have it moved to the In-Progress column.")]
+    public async ValueTask TaskStartCommand(SlashCommandContext context, [Description("ID of the task to start")] [MinMaxValue(minValue: 1)] int id)
+    {
+        var taskItems = await _taskItemRepository.GetAllTaskItemsByGuildIdAsync(context.Guild!.Id);
+        
+        var taskItem = taskItems.GetTaskItemByIdOrDefault(BoardStatus.Backlog, id);
+        
+        if (taskItem is null)
+        {
+            var notFoundEmbed = new DiscordEmbedBuilder()
+                .WithDefaultColor()
+                .WithDescription(
+                    $"A task with the given ID `{id}` was not found.");
+            
+            await context.RespondAsync(notFoundEmbed);
+            return;
+        }
+
+        taskItem.Status = BoardStatus.InProgress;
+        taskItem.LastUpdatedAt = DateTime.UtcNow;
+        
+        await _taskItemRepository.UpdateTaskItemAsync(taskItem);
+
+        var commands = await context.Client.GetGlobalApplicationCommandsAsync();
+        
+        var embed = new DiscordEmbedBuilder()
+            .WithDefaultColor()
+            .WithDescription(
+                $"The task has been started and moved to the In Progress column. View it using {commands.GetMention(["board"])}.");
+        
+        await context.RespondAsync(embed);
+    }
+}
