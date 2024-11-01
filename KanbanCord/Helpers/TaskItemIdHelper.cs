5783bc987d79858f35938f3654823f6c3f9e7822
Jasper Christiansen <jasper@jazper.dk>
2024-11-01 19:07:35 +0100

    Check if task id is in range

--- KanbanCord/Helpers/TaskItemIdHelper.cs
+++ KanbanCord/Helpers/TaskItemIdHelper.cs
@@ -7,6 +7,9 @@ public static class TaskItemIdHelper
     public static TaskItem? GetTaskItemByIdOrDefault(this IReadOnlyList<TaskItem> tasks, BoardStatus boardStatus, int taskId)
     {
         var columnTasks = tasks.Where(x => x.Status == boardStatus).ToList();
+
+        if (columnTasks.Count == 0 || taskId - 1 > columnTasks.Count)
+            return null;
         
         return columnTasks[taskId - 1];
     }
