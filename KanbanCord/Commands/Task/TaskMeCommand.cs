3085c014431107daece441296af8b6f81b145c88
Jasper Christiansen <jasper@jazper.dk>
2024-11-06 00:29:59 +0100

    Use "task user" command logic for "task me" command

--- KanbanCord/Commands/Task/TaskMeCommand.cs
+++ KanbanCord/Commands/Task/TaskMeCommand.cs
@@ -12,24 +12,5 @@ partial class TaskCommandGroup
 {
     [Command("me")]
     [Description("Displays all the tasks assigned to you.")]
-    public async ValueTask TaskMeCommand(SlashCommandContext context)
-    {
-        var boardItems = await _taskItemRepository.GetAllTaskItemsByGuildIdAsync(context.Guild!.Id);
-        
-        var embed = new DiscordEmbedBuilder()
-            .WithDefaultColor()
-            .WithAuthor("KanbanCord Board")
-            .WithDescription($"Tasks assigned to {context.User.Mention}");
-        
-        var backlogString = await boardItems.GetBoardTaskString(context.Client, BoardStatus.Backlog, context.User.Id);
-        embed.AddField("Backlog", backlogString);
-        
-        var inProgressString = await boardItems.GetBoardTaskString(context.Client, BoardStatus.InProgress, context.User.Id);
-        embed.AddField("In Progress", inProgressString);
-        
-        var compltedString = await boardItems.GetBoardTaskString(context.Client, BoardStatus.Completed, context.User.Id);
-        embed.AddField("Completed", compltedString);
-
-        await context.RespondAsync(embed);
-    }
+    public async ValueTask TaskMeCommand(SlashCommandContext context) => await TaskUserCommand(context, context.User);
 }
