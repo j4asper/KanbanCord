c80a5dbd44ee118223e46b9c8b13bd2714dbdd12
Jasper Christiansen <jasper@jazper.dk>
2024-11-26 13:25:59 +0100

    Moved repository and license url to constants

--- KanbanCord.Bot/Commands/RepositoryCommand.cs
+++ KanbanCord.Bot/Commands/RepositoryCommand.cs
@@ -3,14 +3,12 @@ using DSharpPlus.Commands;
 using DSharpPlus.Commands.Processors.SlashCommands;
 using DSharpPlus.Entities;
 using KanbanCord.Bot.Extensions;
+using KanbanCord.Core.Constants;
 
 namespace KanbanCord.Bot.Commands;
 
 public class RepositoryCommand
 {
-    private const string RepositoryUrl = "https://github.com/j4asper/KanbanCord";
-    private const string LicenseUrl = "https://github.com/j4asper/KanbanCord?tab=GPL-3.0-1-ov-file";
-    
     [Command("repository")]
     [Description("Get the repository url for this bot and other information.")]
     public async ValueTask ExecuteAsync(SlashCommandContext context)
@@ -18,11 +16,11 @@ public class RepositoryCommand
         var embed = new DiscordEmbedBuilder()
             .WithDefaultColor()
             .WithAuthor("KanbanCord Repository")
-            .WithDescription($"**KanbanCord** is an open-source Discord bot licensed under [GPL-3.0]({LicenseUrl}). You can host it yourself, explore the code, or contribute by adding features and fixing bugs. The github repository link is available below.");
+            .WithDescription($"**KanbanCord** is an open-source Discord bot licensed under [GPL-3.0]({Source.LicenseUrl}). You can host it yourself, explore the code, or contribute by adding features and fixing bugs. The github repository link is available below.");
         
         var response = new DiscordMessageBuilder()
             .AddEmbed(embed)
-            .AddComponents(new DiscordLinkButtonComponent(RepositoryUrl, "Github Repository"));
+            .AddComponents(new DiscordLinkButtonComponent(Source.RepositoryUrl, "Github Repository"));
         
         await context.RespondAsync(response);
     }
---
+++ KanbanCord.Core/Constants/Source.cs
@@ -0,0 +1,7 @@
+namespace KanbanCord.Core.Constants;
+
+public class Source
+{
+    public const string RepositoryUrl = "https://github.com/j4asper/KanbanCord";
+    public const string LicenseUrl = "https://github.com/j4asper/KanbanCord?tab=GPL-3.0-1-ov-file";
+}
