223f666052403e81cd1a2890e292b72f67c551ae
Jasper <jasper@jazper.dk>
2024-12-31 14:23:10 +0100

    Added option to remove assigned user

--- KanbanCord.Bot/Commands/Task/TaskAssignCommand.cs
+++ KanbanCord.Bot/Commands/Task/TaskAssignCommand.cs
@@ -16,58 +16,79 @@ partial class TaskCommandGroup
     public async ValueTask TaskAssignCommand(
         SlashCommandContext context,
         [Description("Search for the task to select")] [SlashAutoCompleteProvider<AllTaskItemsAutoCompleteProvider>] string task,
-        [Description("The user to assign this task to")] DiscordUser assignee)
+        [Description("The user to assign this task to, leave empty to remove assignee")] DiscordUser? assignee = null)
     {
         var taskItem = await _taskItemRepository.GetTaskItemByObjectIdOrDefaultAsync(new ObjectId(task));
+
+        var embed = new DiscordEmbedBuilder()
+            .WithDefaultColor();
         
         if (taskItem is null)
         {
-            var notFoundEmbed = new DiscordEmbedBuilder()
-                .WithDefaultColor()
-                .WithDescription(
-                    "The selected task was not found, please try again.");
+            embed.WithDescription("The selected task was not found, please try again.");
             
-            await context.RespondAsync(notFoundEmbed);
+            await context.RespondAsync(embed);
             return;
         }
 
-        taskItem.AssigneeId = assignee.Id;
-        taskItem.LastUpdatedAt = DateTime.UtcNow;
-        
-        await _taskItemRepository.UpdateTaskItemAsync(taskItem);
-
-        bool directMessageSentToAssignee;
-
-        try
+        if (assignee is null)
         {
-            var assigneeEmbed = new DiscordEmbedBuilder()
-                .WithDefaultColor()
-                .WithAuthor(context.Guild!.Name, iconUrl: context.Guild.IconUrl)
-                .WithTitle("You have been assigned to a Task!")
-                .AddField("Task Name", taskItem.Title)
-                .AddField("Task Description", taskItem.Description);
+            if (taskItem.AssigneeId is null)
+            {
+                embed.WithDescription("The selected task has no user assigned.");
             
-            await assignee.SendMessageAsync(assigneeEmbed);
-
-            directMessageSentToAssignee = true;
+                await context.RespondAsync(embed);
+                return;
+            }
+            
+            var assignedUser = await context.Client.GetUserAsync(taskItem.AssigneeId!.Value);
+            
+            taskItem.AssigneeId = null;
+            taskItem.LastUpdatedAt = DateTime.UtcNow;
+            
+            await _taskItemRepository.UpdateTaskItemAsync(taskItem);
+            
+            embed.WithDescription($"The task \"{taskItem.Title}\" is no longer assigned to {assignedUser.Mention}.");
+            
+            await context.RespondAsync(embed);
         }
-        catch (Exception)
+        else
         {
-            directMessageSentToAssignee = false;
-        }
+            taskItem.AssigneeId = assignee.Id;
+            taskItem.LastUpdatedAt = DateTime.UtcNow;
+        
+            await _taskItemRepository.UpdateTaskItemAsync(taskItem);
 
-        var embed = new DiscordEmbedBuilder()
-            .WithDefaultColor()
-            .WithDescription(
-                $"The task \"{taskItem.Title}\" has been assigned to {assignee.Mention}.");
+            bool directMessageSentToAssignee;
+
+            try
+            {
+                var assigneeEmbed = new DiscordEmbedBuilder()
+                    .WithDefaultColor()
+                    .WithAuthor(context.Guild!.Name, iconUrl: context.Guild.IconUrl)
+                    .WithTitle("You have been assigned to a Task!")
+                    .AddField("Task Name", taskItem.Title)
+                    .AddField("Task Description", taskItem.Description);
+            
+                await assignee.SendMessageAsync(assigneeEmbed);
+
+                directMessageSentToAssignee = true;
+            }
+            catch (Exception)
+            {
+                directMessageSentToAssignee = false;
+            }
+
+            embed.WithDescription($"The task \"{taskItem.Title}\" has been assigned to {assignee.Mention}.");
         
-        var response = new DiscordInteractionResponseBuilder()
-            .AddMention(new UserMention(assignee))
-            .AddEmbed(embed);
+            var response = new DiscordInteractionResponseBuilder()
+                .AddMention(new UserMention(assignee))
+                .AddEmbed(embed);
         
-        if (!directMessageSentToAssignee)
-            response.WithContent(assignee.Mention);
+            if (!directMessageSentToAssignee)
+                response.WithContent(assignee.Mention);
         
-        await context.Interaction.CreateResponseAsync(DiscordInteractionResponseType.ChannelMessageWithSource, response);
+            await context.Interaction.CreateResponseAsync(DiscordInteractionResponseType.ChannelMessageWithSource, response);
+        }
     }
 }
