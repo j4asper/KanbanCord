c5c32b24d683da0d1ea9b14713a0d654463fb74a
Jasper <jasper@jazper.dk>
2025-03-05 19:54:20 +0100

    Implemented generic host

--- KanbanCord.Bot/Program.cs
+++ KanbanCord.Bot/Program.cs
@@ -1,38 +1,27 @@
-﻿using DSharpPlus;
-using DSharpPlus.Entities;
-using KanbanCord.Bot.Extensions;
-using KanbanCord.Core.Helpers;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using MongoDB.Driver;
+﻿using KanbanCord.Bot.Extensions;
+using Microsoft.Extensions.Hosting;
 
 namespace KanbanCord.Bot;
 
-class Program
+internal class Program
 {
     private static async Task Main()
     {
-        var serviceProvider = new ServiceCollection()
-            .AddDiscordConfiguration()
-            .AddServices()
-            .AddDatabase()
-            .BuildServiceProvider();
-        
-        // Create database collections if they are missing
-        await DatabaseSetupHelper.CreateRequiredCollectionsAsync(
-            serviceProvider.GetRequiredService<IMongoDatabase>(),
-            serviceProvider.GetRequiredService<ILogger<Program>>());
-        
-        var discordClient = serviceProvider.GetRequiredService<DiscordClient>();
+        var builder = Host.CreateDefaultBuilder();
 
-        DiscordActivity status = new("out for work", DiscordActivityType.Watching);
+        builder.UseDefaultServiceProvider(options =>
+        {
+            options.ValidateScopes = true;
+            options.ValidateOnBuild = true;
+        });
         
-        discordClient.Logger.LogInformation($"Application Version: {EnvironmentHelpers.GetApplicationVersion()}");
+        builder
+            .UseConsoleLifetime()
+            .AddHostDependencies()
+            .UseSerilog();
         
-        await discordClient.ConnectAsync(status, DiscordUserStatus.Online);
-
-        discordClient.Logger.LogInformation($"Bot User: {discordClient.CurrentUser.Username} ({discordClient.CurrentUser.Id})");
+        var app = builder.Build();
         
-        await Task.Delay(-1);
+        await app.RunAsync();
     }
 }
