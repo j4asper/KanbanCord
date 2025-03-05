82fc28858f6f9cac82d85760a988808ef21f89f0
Jasper <jasper@jazper.dk>
2025-03-05 19:52:25 +0100

    Added HostBuilderExtensions

---
+++ KanbanCord.Bot/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace KanbanCord.Bot.Extensions;
+
+public static class HostBuilderExtensions
+{
+    public static IHostBuilder AddHostDependencies(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureServices((hostContext, services) =>
+        {
+            services.AddServices(hostContext.Configuration);
+            services.AddDiscordConfiguration(hostContext.Configuration);
+        });
+        
+        return hostBuilder;
+    }
+    
+    public static IHostBuilder UseSerilog(this IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureLogging((hostContext, logging) =>
+        {
+            logging.ClearProviders();
+            
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(hostContext.Configuration)
+                .CreateLogger();
+            
+            logging.AddSerilog();
+        });
+        
+        return hostBuilder;
+    }
+}
